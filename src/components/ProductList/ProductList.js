3c8989c23df0f73446232dc57d077b2e28a56073
mjaumi <mjaumi2864@gmail.com>
2023-07-29 17:53:05 +0600

    product list responsiveness done

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -12,25 +12,25 @@ const ProductList = () => {
 
     // rendering the product list component here
     return (
-        <section className='w-4/5 mx-auto my-24'>
-            <div className='flex justify-between items-center'>
+        <section className='w-[90%] md:w-4/5 mx-auto my-24'>
+            <div className='flex flex-col md:flex-row text-center md:text-left justify-center md:justify-between items-center'>
                 <div>
                     <h1 className='font-spectral text-3xl font-bold'>Shop by Category</h1>
                     <p className='text-lg mt-6'>Life is hard enough already. Let us <br /> make it a little easier.</p>
                 </div>
-                <div className='flex'>
+                <div className='flex mt-8 md:mt-0'>
                     <SSAngleButtonLeft clickHandler={prevHandler} />
                     <SSAngleButtonRight clickHandler={nextHandler} />
                 </div>
             </div>
-            <div className='mt-16 px-10'>
+            <div className='mt-16 md:px-10'>
                 <Swiper
                     ref={ref}
                     slidesPerView={1}
                     className='mySwiper'
                 >
                     <SwiperSlide>
-                        <div className='grid grid-cols-3 gap-x-8 gap-y-14'>
+                        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-8 gap-y-14'>
                             <ProductListCard />
                             <ProductListCard />
                             <ProductListCard />
@@ -43,7 +43,7 @@ const ProductList = () => {
                         </div>
                     </SwiperSlide>
                     <SwiperSlide>
-                        <div className='grid grid-cols-3 gap-x-8 gap-y-14'>
+                        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-8 gap-y-14'>
                             <ProductListCard />
                             <ProductListCard />
                             <ProductListCard />
